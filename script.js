f88b6a463a95694c2a501b4c502b5b6762773842
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2025-07-28 19:53:05 -0300

    Aula 28/07

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ for (let i = 0; i<linguagens.length; i++){
     li.textContent = linguagens[i];
     item.appendChild(li);
 }
-    
+
+
+const objeto =  document.querySelector("#aluno");
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
+
+const divDetalhes = document.querySelector("#detalhes-aluno");
+
+let htmlConteudo = `
+  <ul>
+    <li><strong>Nome:</strong> ${aluno1.nome}</li>
+    <li><strong>Idade:</strong> ${aluno1.idade} anos</li>
+    <li><strong>Ano Letivo:</strong> ${aluno1.anoLetivo}</li>
+    <li><strong>Matérias Favoritas:</strong> ${aluno1.materiasFavoritas.join(", ")}</li>
+  </ul>
+`;
+
+divDetalhes.innerHTML = htmlConteudo;
